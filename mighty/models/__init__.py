04901b4eb314ce8ade0dc5265ec6197bc6b7cbc3
dizcza <dizcza@gmail.com>
2020-09-23 15:17:24 +0200

    SerializableModule

--- mighty/models/__init__.py
+++ mighty/models/__init__.py
@@ -1,3 +1,4 @@
 from .autoencoder import AutoencoderLinear, AutoencoderOutput
 from .flatten import Flatten, Reshape
 from .mlp import MLP
+from .serialize import SerializableModule
---
+++ mighty/models/serialize.py
@@ -0,0 +1,30 @@
+from abc import ABC
+
+import torch.nn as nn
+
+
+class SerializableModule(nn.Module, ABC):
+    state_attr = []
+
+    def state_dict(self, destination=None, prefix='', keep_vars=False):
+        destination = super().state_dict(destination=destination,
+                                         prefix=prefix, keep_vars=keep_vars)
+        for attribute in self.state_attr:
+            destination[prefix + attribute] = getattr(self, attribute)
+        return destination
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys,
+                              error_msgs):
+        state_dict_keys = list(state_dict.keys())
+        for attribute in self.state_attr:
+            key = prefix + attribute
+            if key in state_dict_keys:
+                setattr(self, attribute, state_dict.pop(key))
+            elif strict:
+                missing_keys.append(key)
+        super()._load_from_state_dict(state_dict=state_dict, prefix=prefix,
+                                      local_metadata=local_metadata,
+                                      strict=strict, missing_keys=missing_keys,
+                                      unexpected_keys=unexpected_keys,
+                                      error_msgs=error_msgs)
