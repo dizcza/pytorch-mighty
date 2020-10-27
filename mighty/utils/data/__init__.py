6d8761ef9c195bc752d68b9bbb657ce0dac6f1a1
dizcza <dizcza@gmail.com>
2020-10-27 22:21:53 +0100

    removed caltech dataset

--- mighty/utils/data/__init__.py
+++ mighty/utils/data/__init__.py
@@ -1,4 +1,3 @@
 from .loader import DataLoader
-from .caltech import Caltech10, Caltech256
 from .normalize import NormalizeInverse, get_normalize_inverse
 from .transforms_default import TransformDefault
